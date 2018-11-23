f440dd67b910be3238943029edd833929c4a9164
Arne De Smedt <arnedesmdt@gmail.com>
2018-11-23 18:21:59 +0100

    enable postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,18 +2,18 @@ module.exports = {
     plugins: {
         autoprefixer: {},
         tailwindcss: './tailwind.config.js',
-        // '@fullhuman/postcss-purgecss': {
-        //     content: [
-        //         './src/components/*.vue',
-        //         './node_modules/vue-ads-pagination/dist/vue-ads-pagination.common.js',
-        //     ],
-        //     whitelistPatterns: [
-        //         /^body$/,
-        //         /^html$/,
-        //         /^vue-ads-w-\d{1,3}$/,
-        //         /^fa-sort-(.*)$/,
-        //     ],
-        // },
+        '@fullhuman/postcss-purgecss': {
+            content: [
+                './src/components/*.vue',
+                './node_modules/vue-ads-pagination/dist/vue-ads-pagination.common.js',
+            ],
+            whitelistPatterns: [
+                /^body$/,
+                /^html$/,
+                /^vue-ads-w-\d{1,3}$/,
+                /^fa-sort-(.*)$/,
+            ],
+        },
         'postcss-import': {},
         'postcss-url': {},
     },
