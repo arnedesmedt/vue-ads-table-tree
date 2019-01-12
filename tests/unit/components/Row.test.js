505ed59558ebf37faca3842c0eee95716b3bb1ea
Arne De Smedt <arnedesmdt@gmail.com>
2019-01-12 18:06:30 +0100

    add row tests

---
+++ tests/unit/components/Row.test.js
@@ -0,0 +1,69 @@
+import { shallowMount } from '@vue/test-utils';
+
+import RowComponent from '../../../src/components/Row';
+import Row from '../../../src/models/Row';
+import Column from '../../../src/models/Column';
+import ColumnCollection from '../../../src/collections/ColumnCollection';
+import ClassProcessor from '../../../src/services/ClassProcessor';
+
+describe('Row', () => {
+    let component;
+    let row;
+    let columns;
+    let columnA;
+    let columnB;
+
+
+    beforeEach(() => {
+        row = new Row({
+            firstName: 'Arne',
+            lastName: 'De Smedt',
+        });
+
+        columnA = new Column({
+            property: 'firstName',
+        });
+        columnB = new Column({
+            property: 'lastName',
+        });
+
+        columns = new ColumnCollection([
+            columnA,
+            columnB,
+        ]);
+
+        component = shallowMount(RowComponent, {
+            propsData: {
+                row,
+                columns,
+                classes: new ClassProcessor({}, 0),
+            },
+        });
+    });
+
+    it('renders a row', () => {
+        expect(component.html()).toMatchSnapshot();
+    });
+
+    it('selects the correct column slot', () => {
+        component.setProps({
+            slots: {
+                firstName: 'test',
+                lastName: 'test2',
+            },
+        });
+
+        expect(component.vm.columnSlot(columnA)).toBe('test');
+    });
+
+    it('selects the correct cell slot', () => {
+        component.setProps({
+            slots: {
+                firstName_Arne: 'testcell',
+                firstName: 'test',
+            },
+        });
+
+        expect(component.vm.columnSlot(columnA)).toBe('testcell');
+    });
+});
