c5ea626482c31467215898276b99cc634676a6b1
Arne De Smedt <arnedesmdt@gmail.com>
2019-02-15 17:40:15 +0100

    enable purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,20 +2,20 @@ module.exports = {
     plugins: {
         autoprefixer: {},
         tailwindcss: './tailwind.config.js',
-        // '@fullhuman/postcss-purgecss': {
-        //     content: [
-        //         './src/components/*.vue',
-        //         './src/services/*.js',
-        //         './node_modules/vue-ads-pagination/dist/vue-ads-pagination.common.js',
-        //         './node_modules/vue-ads-form-builder/dist/vue-ads-form-builder.common.js',
-        //     ],
-        //     whitelistPatterns: [
-        //         /^body$/,
-        //         /^html$/,
-        //         /^fa-sort-(.*)$/,
-        //         /^vue-ads-w-(\d+)\/5$/,
-        //     ],
-        // },
+        '@fullhuman/postcss-purgecss': {
+            content: [
+                './src/components/*.vue',
+                './src/services/*.js',
+                './node_modules/vue-ads-pagination/dist/vue-ads-pagination.common.js',
+                './node_modules/vue-ads-form-builder/dist/vue-ads-form-builder.common.js',
+            ],
+            whitelistPatterns: [
+                /^body$/,
+                /^html$/,
+                /^fa-sort-(.*)$/,
+                /^vue-ads-w-(\d+)\/4$/,
+            ],
+        },
         'postcss-import': {},
         'postcss-url': {},
     },
