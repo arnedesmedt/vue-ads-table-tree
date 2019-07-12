f13f1f24f26d42f23bc76799b5928483593fc119
Arne De Smedt <arnedesmdt@gmail.com>
2019-07-12 23:36:57 +0200

    removed accidently purge css

--- postcss.config.js
+++ postcss.config.js
@@ -2,20 +2,18 @@ module.exports = {
     plugins: {
         autoprefixer: {},
         tailwindcss: './tailwind.config.js',
-        // '@fullhuman/postcss-purgecss': {
-        //     content: [
-        //         './src/components/*.vue',
-        //         './src/services/*.js',
-        //         './node_modules/vue-ads-pagination/dist/vue-ads-pagination.common.js',
-        //         './node_modules/vue-ads-form-builder/dist/vue-ads-form-builder.common.js',
-        //     ],
-        //     whitelistPatterns: [
-        //         /^body$/,
-        //         /^html$/,
-        //         /^fa-sort(.*)$/,
-        //         /^vue-ads-w-(\d+)\/4$/,
-        //     ],
-        // },
+        '@fullhuman/postcss-purgecss': {
+            content: [
+                './src/components/*.vue',
+                './src/services/*.js',
+                './node_modules/vue-ads-pagination/dist/vue-ads-pagination.common.js',
+                './node_modules/vue-ads-form-builder/dist/vue-ads-form-builder.common.js',
+            ],
+            whitelistPatterns: [
+                /^fa-sort(.*)$/,
+                /^vue-ads-w-(\d+)\/4$/,
+            ],
+        },
         'postcss-import': {},
         'postcss-url': {},
     },
